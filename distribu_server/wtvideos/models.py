425377eff3a23c327bdc2d0db1131d73f5bfdf85
Sawyer Hood <sawyerjhood@gmail.com>
2016-02-07 21:33:02 -0500

    moved location of the media in django settings

--- distribu_server/wtvideos/models.py
+++ distribu_server/wtvideos/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 class Video(models.Model):
     title = models.CharField(max_length=140)
